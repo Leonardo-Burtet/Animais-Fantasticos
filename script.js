c5812a84cf894199d48ca51137fd8d6b1ac90030
Leonardo Burtet <leonardoburtet@outlook.com>
2021-08-13 17:34:06 -0300

    add animacao scroll

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function initTabNav() {
   const tabMenu = document.querySelectorAll(".js-tabmenu li");
   const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-  if (tabMenu.length) {
+  if (tabMenu.length && tabContent.length) {
     tabContent[0].classList.add("ativo");
 
     function activeTab(index) {
@@ -31,7 +31,7 @@ function initAccordion() {
     accordionList[0].nextElementSibling.classList.add(activeClass);
 
     function activeAccordion() {
-      this.classList.add(activeClass);
+      this.classList.toggle(activeClass);
       this.nextElementSibling.classList.toggle(activeClass);
     }
     accordionList.forEach((item) => {
@@ -40,3 +40,56 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
+    // forma alternativa
+    // const topo = section.offsetTop;
+    //window.scrollTo({
+    //  top: topo,
+    //  behavior: "smooth",
+    //});
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
+initScrollSuave();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initAnimacaoScroll();
