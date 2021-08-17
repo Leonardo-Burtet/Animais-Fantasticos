9fd98ecf43fe0b6fe2caba479147d9710da4f527
Leonardo Burtet <leonardoburtet@outlook.com>
2021-08-17 13:57:07 -0300

    refactor add modules

---
+++ js/initAccordion.js
@@ -0,0 +1,17 @@
+export default function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "ativo";
+
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initTabNav from "./initTabNav.js";
+import initAccordion from "./initAccordion.js";
+import initScrollSuave from "./initScrollSuave.js";
+import initAnimacaoScroll from "./initAnimacaoScroll.js";
+
+initTabNav();
+initAccordion();
+initScrollSuave();
+initAnimacaoScroll();
