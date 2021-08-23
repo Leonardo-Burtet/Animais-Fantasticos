fd90c0c8381f3f0966fa431f719e8dec1dfc2e53
Leonardo Burtet <leonardoburtet@outlook.com>
2021-08-23 19:19:06 -0300

    add animacao numeros

--- js/modules/animacaoScroll.js
+++ js/modules/animacaoScroll.js
@@ -10,7 +10,7 @@ export default function initAnimacaoScroll() {
         const isSectionVisible = sectionTop - windowMetade < 0;
         if (isSectionVisible) {
           section.classList.add("ativo");
-        } else {
+        } else if (section.classList.contains("ativo")) {
           section.classList.remove("ativo");
         }
       });
--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ import initModal from "./modules/modal.js";
 import initToolTip from "./modules/tooltip.js";
 import initDropDownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
+import initAnimaNumeros from "./modules/animaNumeros.js";
 
 initTabNav();
 initAccordion();
@@ -15,3 +16,4 @@ initModal();
 initToolTip();
 initDropDownMenu();
 initMenuMobile();
+initAnimaNumeros();
